48f695951a37d62b6f604c041f7774382d218619
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-18 20:47:04 +0530

    Add files via upload

--- Employee.java
+++ Employee.java
@@ -1,12 +1,22 @@
-
-public class Employee extends Member {
-
-
-    public void emp() {
-
-      String department="IT";
-        System.out.print("Department of the emoployee: "+department+"\n");
-
-
+public class Employee{
+    public void emp1() {
+        String name1 = "Robert";
+        int yoj = 1994;
+        String add = "64C- WallsStreat";
+        System.out.println(name1 + "         " + yoj + "           " + add);
     }
+    public void emp2() {
+        String name2 = "Sam";
+        int yoj2 = 2000;
+        String add2 = "64D- WallsStreat";
+        System.out.println(name2 + "            " + yoj2 + "           " + add2);
+    }
+    public void emp3() {
+        String name3="John";
+        int yoj3=1999;
+        String add3="26B- WallsStreat";
+        System.out.println(name3+"           "+yoj3+"           "+add3);
+    }
+
+
 }
---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Name      Year of joining      Address");
+        Employee e=new Employee();
+        e.emp1();
+        e.emp2();
+        e.emp3();
+    }
+}
+
+
