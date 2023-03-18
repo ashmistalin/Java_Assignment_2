dd3a76960ea1e1d5c9fce7d390a5fd3295748876
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-18 20:27:40 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -6,3 +6,11 @@ public class Solution{
         bird.sing();
     }
 }
+
+```
+## OUTPUT:
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=3980:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java java\out\production\java java" Solution
+I am walking
+I am flying I am singing
+Process finished with exit code 0
+```
