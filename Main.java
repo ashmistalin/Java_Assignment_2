688ed744e8a36b563e4c05042fbfd59ac6bf9b56
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-18 20:47:51 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,3 +10,14 @@ public class Main {
 }
 
 
+## OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=4572:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java java\out\production\java java" Main
+Name      Year of joining      Address
+Robert         1994           64C- WallsStreat
+Sam            2000           64D- WallsStreat
+John           1999           26B- WallsStreat
+
+Process finished with exit code 0
+
+
