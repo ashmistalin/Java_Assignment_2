57389bb50c45fcce27cc8326c480c6c8f1b3d8f3
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-20 14:14:30 +0530

    Update Power.java

--- Power.java
+++ Power.java
@@ -29,5 +29,14 @@ public class Power {
 }
 
 
+## OUTPUT:
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=13490:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java full stack\out\production\java full stack" Power
+Enter the base:
+2
+Enter the exponent:
+3
+Result: 8
+
+Process finished with exit code 0
 
 
