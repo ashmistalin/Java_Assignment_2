ef2564a29917472afb6013245377fc2e31b560ba
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-21 08:16:02 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -1,3 +1,8 @@
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
 public class Solution{
     public static void main(String[] args){
         Bird bird = new Bird();
