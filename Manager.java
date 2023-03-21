980a946598df9585ec6e7abe39d3ea585cba45c0
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-21 08:22:24 +0530

    Delete Manager.java

--- Manager.java
+++
@@ -1,9 +0,0 @@
-
-public class Manager extends Member {
-    public void emp1() {
-
-       String specialization="Front end developer.";
-        System.out.println("specialization:"+specialization);
-
-    }
-}
