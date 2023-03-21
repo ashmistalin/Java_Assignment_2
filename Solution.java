d5a9b3849df0663e80ff5fb4e540f3ebb21af2a8
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-21 08:16:36 +0530

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -3,6 +3,17 @@ class Animal{
         System.out.println("I am walking");
     }
 }
+class Bird extends Animal {
+    void fly() {
+        System.out.print("I am flying ");
+    }
+    void sing()
+    {
+        System.out.print("I am singing");
+    }
+
+}
+
 public class Solution{
     public static void main(String[] args){
         Bird bird = new Bird();
