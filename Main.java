f749ccc037663c2b06e6d2e85e46b7521979d334
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-21 08:24:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,29 @@
 
+public class Employees{
+    public void emp1() {
+        String name1 = "Robert";
+        int yoj = 1994;
+        String add = "64C- WallsStreat";
+        System.out.println(name1 + "         " + yoj + "           " + add);
+    }
+    public void emp2() {
+        String name2 = "Sam";
+        int yoj2 = 2000;
+        String add2 = "64D- WallsStreat";
+        System.out.println(name2 + "            " + yoj2 + "           " + add2);
+    }
+    public void emp3() {
+        String name3="John";
+        int yoj3=1999;
+        String add3="26B- WallsStreat";
+        System.out.println(name3+"           "+yoj3+"           "+add3);
+    }
+
+
+}
+
+Main function:
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Name      Year of joining      Address");
